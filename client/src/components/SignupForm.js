13be1e6fe3813c38cbe5ef38fab816ab8361a451
Diana Vu <aznalive@hotmail.com>
2022-11-19 22:51:43 -0500

    test

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -6,12 +6,10 @@ import { ADD_USER } from '../utils/mutations'
 import Auth from '../utils/auth'
 
 const SignupForm = () => {
-  // set initial form state
   const [userFormData, setUserFormData] = useState({username: '', email: '', password: '' })
-  // set state for form validation
   const [validated] = useState(false)
-  // set state for alert
   const [showAlert, setShowAlert] = useState(false)
+
   // declared the addUser with the useMutation
   const [addUser, { error }] = useMutation(ADD_USER)
   const handleInputChange = event => {
@@ -21,7 +19,6 @@ const SignupForm = () => {
 
   const handleFormSubmit = async event => {
     event.preventDefault()
-    // check if form has everything (as per react-bootstrap docs)
     const form = event.currentTarget
     if (form.checkValidity() === false) {
       event.preventDefault()
